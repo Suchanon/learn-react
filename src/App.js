f0b9f0a8fbe85d7b722f552f8b3e4a2a0ec5d3d6
Bank <suchanon66@gmail.com>
2022-04-28 17:14:30 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+const styles = {
+  color: 'blue',
+  fontStyle: 'italic',
+}
+
+const str = <h1 style={ styles }>Hello JSX</h1>
+
+const error = true
+
+// function greate(name){
+//   return `Hello func ${name}`
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {str} 
+      {/* {greate('Taste that ger')} */}
+      <label> name <input /></label>
+      {(error) ? <div style={{ color: 'red'}}>Invalid name </div> : null}
     </div>
   );
 }
